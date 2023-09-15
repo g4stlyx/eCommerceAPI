00187bdee567bb9ebd326de5202b469575e77e94
Sefa Ağardan <54821580+g4stlyx@users.noreply.github.com>
2023-09-15 17:13:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const express = require("express")
 const app = express()
 
 // rest of the packages
-const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
 const path = require("path")
@@ -43,16 +42,10 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitizer())
 
-app.use(morgan("tiny")) // http request logger
 app.use(express.json()) // req.body
 app.use(cookieParser(process.env.JWT_SECRET)) // req.cookies - req.signedCookies
 app.use(express.static(path.join(__dirname,"/public"))) // static files
 app.use(fileUpload()) // req.files
-
-app.get("/",(req,res)=>{
-    console.log(req.signedCookies)
-    res.send("e-commerce api")
-})
 
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/users",userRouter)
@@ -62,9 +55,6 @@ app.use("/api/v1/orders",orderRouter)
 
 app.use(notFoundMiddleware) // 404
 app.use(errorHandlerMiddleware) // other errors
-
-
-
 
 const port = process.env.PORT || 5000
 const start = async () => {
